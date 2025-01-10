62694f5e944bc2e02501a1f26512b569f816ec05
Mirko Mälicke <mirko@hydrocode.de>
2025-01-10 19:46:49 +0100

    setup a custom CREATIVE entries page

--- src/creative_uploader.py
+++ src/creative_uploader.py
@@ -1,8 +1,9 @@
-from fastapi import Request
+from fastapi import Request, APIRouter
 from starlette.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
+from metacatalog_api import core
 from metacatalog_api.server import app, server
 from metacatalog_api.router.api.read import read_router as api_read_router
 from metacatalog_api.router.api.create import create_router as api_create_router
@@ -22,6 +23,9 @@ app.add_middleware(
 # create a template engine for creative only
 creative_templates = Jinja2Templates(directory=Path(__file__).parent / 'templates')
 
+# create a router for extra CREATIVE pages
+creative_pages = APIRouter()
+
 # the main page is defined here
 # this can easily be changed to a different entrypoint
 @app.get('/')
@@ -33,6 +37,16 @@ def index(request: Request):
     return creative_templates.TemplateResponse(request=request, name="index.html", context={"path": server.app_prefix})
 
 
+# we replace some of the default explorer pages with our own
+@creative_pages.get('/creative/entries.html')
+def entries_list(request: Request, limit: int = None, offset: int = None):
+    entries = core.entries(limit=limit, offset=offset)
+    return creative_templates.TemplateResponse(request=request, name="entries.html", context={"path": server.app_prefix, "entries": entries})
+
+@creative_pages.get('/page/entries.html')
+def get_entries_page(request: Request):
+    return creative_templates.TemplateResponse(request=request, name="entries_page.html", context={"path": server.app_prefix})
+
 # add the API
 app.include_router(api_read_router)
 app.include_router(api_create_router)
@@ -41,6 +55,7 @@ app.include_router(api_create_router)
 app.mount(f"{server.app_prefix}static", static_files, name="static")
 app.include_router(explorer_router, prefix=f"/{server.app_name}")
 app.include_router(explorer_create, prefix=f"/{server.app_name}")
+app.include_router(creative_pages, prefix=f"/{server.app_name}")
 
 if __name__ == '__main__':
     # run the server
